2fe1696997fba39ad4e1280b723ff988d8313228
omashasarra <omashasara@gmail.com>
2026-08-11 22:04:45 +0300

    fix(footer): correct footer layout issue

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -158,8 +158,7 @@ export default function Footer() {
                 <span>cedar_stone_lb</span>
               </a>
               <a
-                href="https://www.linkedin.com/company/cedar-stone-lb"
-                target="_blank"
+                href="https://www.linkedin.com/in/cedar-stone-82a909423?utm_source=share_via&utm_content=profile&utm_medium=member_ios
                 rel="noopener noreferrer"
                 className="flex items-center gap-2.5 hover:text-black transition-colors"
               >
